7614f8987b65340ebcbdec8ac3b6b23e45048311
Sebastian Ferro <sebastian@comparaencasa.com>
2017-03-04 11:19:29 -0300

    Initial Commit

---
+++ miniRetoConsole/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  miniRetoConsole
+//
+//  Created by Sebastian Ferro on 2017/03/04.
+//  Copyright © 2017 Sebastian Ferro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
