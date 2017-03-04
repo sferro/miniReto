bd454904eb612cc148db32912a60f882e5abb448
Sebastian Ferro <sebastian@comparaencasa.com>
2017-03-04 11:36:21 -0300

    Version Inicial

--- miniRetoConsole/main.swift
+++ miniRetoConsole/main.swift
@@ -1,12 +1,25 @@
-//
-//  main.swift
-//  miniRetoConsole
-//
-//  Created by Sebastian Ferro on 2017/03/04.
-//  Copyright © 2017 Sebastian Ferro. All rights reserved.
-//
+//: miniReto
+/*
+ Imprimir numeros del 0 al 100
+ Si el numero es divisible entre 5 imprime:  #El numero y la palabra bingo!!!
+ Si el numero es par imprime:  #El numero y la palabra par!!!
+ Si el numero es impar imprime:  #El numero y la palabra impar!!!
+ Si el numero se encuentra entre un rango de 30 al 40 imprime:  #El numero y la palabra Vive Swift!!!
+ 
+ */
 
 import Foundation
 
-print("Hello, World!")
-
+for numero in 0...100 {
+    if numero >= 5 && numero % 5 == 0 {
+        print("#\(numero) Bingo!!!")
+    }
+    if numero % 2 == 0 {
+        print("#\(numero) Par!!!")
+    } else {
+        print("#\(numero) Impar!!!")
+    }
+    if numero >= 30 && numero <= 40 {
+        print("#\(numero) Viva Swift!!!")
+    }
+}
